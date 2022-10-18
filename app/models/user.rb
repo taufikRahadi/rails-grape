faa5b5200d2d63d5e95c3aade39c439d10f8c546
Taufik Rahadi <taufikrahadi1@gmail.com>
2022-10-18 16:28:46 +0700

    using helper has_secure_password and validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :posts
   belongs_to :role, class_name: "Role", foreign_key: "roles_id"
 
-  before_save :hash_password
+  # before_save :hash_password
 
   validates :username, presence: true, uniqueness: { message: 'User already exists' }
+  validates :password, presence: true, length: { minimum: 8 }
 
   private
     def hash_password
