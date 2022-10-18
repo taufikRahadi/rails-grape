b3fc36ba6bef9fcfc832aec787527e74aad38ae0
Taufik Rahadi <taufikrahadi1@gmail.com>
2022-10-18 16:26:04 +0700

    change password => password dont do this at real project

--- db/migrate/20221017115844_create_users.rb
+++ db/migrate/20221017115844_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :username, unique: true, null: false
       t.string :fullname, null: false
-      t.string :password, null: false
+      t.string :password_digest, null: false
       t.timestamps
     end
   end
