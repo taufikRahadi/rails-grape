122590830bd01e91580bc4bdb5005c9a300b5927
Taufik Rahadi <taufikrahadi1@gmail.com>
2022-10-19 10:21:15 +0700

    add user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-roles = ['author', 'editor', 'owner']
+roles = ['user', 'author', 'editor', 'owner']
 roles.each do |i|
   Role.create(name: i)
 end
