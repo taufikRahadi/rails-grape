0b59d396c68d42f17d0630dc19d88642a618412a
Taufik Rahadi <taufikrahadi1@gmail.com>
2022-10-19 14:02:09 +0700

    unused formatter

--- app/controllers/api/v0/auth/routes.rb
+++ app/controllers/api/v0/auth/routes.rb
@@ -1,5 +1,5 @@
 class Api::V0::Auth::Routes < Grape::API
-  formatter :json, SuccessFormatter
+  # formatter :json, SuccessFormatter
   error_formatter :json, ErrorFormatter
 
   mount Api::V0::Auth::Resources::Auth
--- app/controllers/api/v0/user/routes.rb
+++ app/controllers/api/v0/user/routes.rb
@@ -1,5 +1,5 @@
 class Api::V0::User::Routes < Grape::API
-  formatter :json, SuccessFormatter
+  # formatter :json
   error_formatter :json, ErrorFormatter
 
   mount Api::V0::User::Resources::User
