0a8d42d6941ba91a5271a1ac20bce4d55cd7dc4b
Taufik Rahadi <taufikrahadi1@gmail.com>
2022-10-20 15:14:49 +0700

    add mount_uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  mount_uploader :cover, PostCoverUploader
+  
   has_and_belongs_to_many :tags
-  belongs_to :user, class_name: "user", foreign_key: "user_id"
+  belongs_to :author, class_name: "User", foreign_key: :author_id
 end
